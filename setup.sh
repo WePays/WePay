ac4eb85998cc402a9c890a880645961fc9b3b299
Tezigudo <godjangg@gmail.com>
2022-10-19 22:32:45 +0700

    🔨 added coniguration file for dev

---
+++ setup.sh
@@ -0,0 +1,36 @@
+# create virtual environment
+OS="`uname`"
+
+# will change to vitualenv later
+echo "creating virtual environment..."
+case $OS in
+    'Darwin')
+    python3 -m venv env
+    ;;
+    'WindowsNT')
+    python -m venv env
+    ;;
+    *)
+    python -m venv env
+esac
+echo "created"
+
+echo "activating the environment..."
+case $OS in
+    'Darwin')
+    source env/bin/activate
+    ;;
+    'WindowsNT')
+    env\Scripts\activate
+    ;;
+    *)
+    source env/bin/activate
+esac
+echo "activated"
+
+echo installing requirements...
+pip install -r requirements.txt
+python manage.py migrate
+python manage.py loaddata data/*.json
+echo
+echo everything finished
