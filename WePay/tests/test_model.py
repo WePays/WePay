f7fe4bf94b30db9a51b7346c41465fbb813677f5
Tezigudo <godjangg@gmail.com>
2022-10-20 20:46:49 +0700

    ✅ added some test for bill #13

--- WePay/tests/test_model.py
+++ WePay/tests/test_model.py
@@ -1,4 +1,20 @@
-# from django.test import TestCase
+from unittest import skip
+from django.test import TestCase
 
-# from ..models import Bills, Food, Payment, BankPayment
+from ..models import Bills, Food #, Payment, BankPayment
 # Create your tests here.
+
+class BillModelTest(TestCase):
+
+    """test for Bill model"""
+    @skip("unfinished test")
+    def test_calculate_price(self):
+        """test calculate price"""
+
+    @skip("unfinished test")
+    def test_total_price(self):
+        """test total price"""
+
+    @skip("unfinished test")
+    def test_all_user(self):
+        """test all user"""
