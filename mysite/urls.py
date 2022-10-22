25c5ab7d8a4104ef3b2642f7a5004835fc519b70
Tezigudo <godjangg@gmail.com>
2022-10-22 20:29:13 +0700

    🚧 add discord logo and refactor something #16 #14 #19 #18

--- mysite/urls.py
+++ mysite/urls.py
@@ -22,7 +22,7 @@ from .views import signup
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("bill/", include("WePay.urls")),
-    path("account/", include("allauth.urls")),
+    path("accounts/", include("allauth.urls")),
     path("signup/", signup, name="signup"),
     # path('', RedirectView.as_view(url='/accounts/login/')),
     path("", RedirectView.as_view(url="/bill/")),
