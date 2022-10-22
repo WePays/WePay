f7c110b87c5bd422d68b1917f7a2a0308c9ed956
ThanadolU <dolthanadolu@gmail.com>
2022-10-22 22:12:09 +0700

    ✅ add docstring to all tests already #13

--- WePay/tests/test_model.py
+++ WePay/tests/test_model.py
@@ -32,7 +32,7 @@ class BillModelTest(TestCase):
         self.coke = Food.objects.create(title='Coke', price=15, bill=self.bill)
 
     def test_calculate_price(self):
-        """test calculate price"""
+        """test calculate price for each user."""
         self.pepsi.add_user(self.user1)
         self.pepsi.add_user(self.user2)
         self.coke.add_user(user=self.user2)
@@ -40,17 +40,17 @@ class BillModelTest(TestCase):
         self.assertEqual(self.bill.calculate_price(person=self.user2), 25.0)
 
     def test_total_price(self):
-        """test total price"""
+        """test total price in bill."""
         self.assertEqual(self.bill.total_price, 35)
 
     def test_all_user(self):
-        """test all user"""
+        """test all user."""
         self.pepsi.add_user(user=self.user1)
         self.coke.add_user(user=self.user2)
         self.assertListEqual(self.bill.all_user, [self.pepsi.user, self.coke.user])
 
     def test_duplicate_user(self):
-        """test duplicate user"""
+        """test when you add duplicate user."""
         self.pepsi.add_user(user=self.user1)
         self.pepsi.add_user(user=self.user1)
         self.coke.add_user(user=self.user2)
@@ -59,7 +59,7 @@ class BillModelTest(TestCase):
 
 
 class FoodModelTest(TestCase):
-    """test for Food model"""
+    """test for Food model."""
 
     def setUp(self):
         """Setup before running a tests."""
@@ -84,17 +84,20 @@ class FoodModelTest(TestCase):
         self.coke = Food.objects.create(title='Coke', price=15, bill=self.bill)
 
     def test_each_price(self):
+        """test food price for each user."""
         self.pepsi.add_user(user=self.user1)
         self.pepsi.add_user(user=self.user2)
         self.assertEqual(self.pepsi.each_price(), 10)
 
     def test_add_user(self):
+        """test add user to Food.user.all()"""
         self.pepsi.add_user(user=self.user1)
         self.pepsi.add_user(user=self.user2)
         self.assertIn(self.user1, self.pepsi.user.all())
         self.assertIn(self.user2, self.pepsi.user.all())
 
     def test_add_duplicate_user(self):
+        """test add duplicate user to that food."""
         self.pepsi.add_user(user=self.user1)
         self.pepsi.add_user(user=self.user1)
         self.pepsi.add_user(user=self.user2)
