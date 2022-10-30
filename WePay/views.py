3f02ad289bccc38e0eb119e41e24f0ede6771183
Tezigudo <godjangg@gmail.com>
2022-10-30 16:38:08 +0700

    migration

--- WePay/views.py
+++ WePay/views.py
@@ -15,11 +15,11 @@ class BillView(LoginRequiredMixin, generic.ListView):
     template_name = "Wepay/bill.html"
     context_object_name = "my_bill"
 
-    # def get(self, request, *arg, **kwargs):
-    #     user = request.user
-    #     if user.is_authenticated and not UserProfile.objects.filter(user_id=user.id).exists():
-    #         UserProfile.objects.create(user_id=user.id)
-    #     return super().get(request, *arg, **kwargs)
+    def get(self, request, *arg, **kwargs):
+        user = request.user
+        if user.is_authenticated and not UserProfile.objects.filter(user_id=user.id).exists():
+            UserProfile.objects.create(user_id=user.id)
+        return super().get(request, *arg, **kwargs)
 
     def get_queryset(self):
 
