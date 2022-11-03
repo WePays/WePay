c648ec1eda86f74a26a387d81990323136cb97e9
Tezigudo <godjangg@gmail.com>
2022-11-03 10:56:14 +0700

    🗃️ use PolymorphicModel instead of django model #46

--- WePay/models/payment.py
+++ WePay/models/payment.py
@@ -1,12 +1,15 @@
-# from django.contrib import admin
+from django.contrib import admin
 from abc import abstractmethod
 from typing import Any
-from django.db import models
-from django.utils import timezone
-from .userprofile import UserProfile
-from .bill import Bills
 
 import omise
+from django.db import models
+from django.utils import timezone
+from polymorphic.models import PolymorphicModel
+# from polymorphic.admin import StackedPolymorphicInline, PolymorphicInlineSupportMixin
+
+from .bill import Bills
+from .userprofile import UserProfile
 
 OMISE_PUBLIC = 'pkey_test_5tgganhu45npoycv190'
 OMISE_SECRET = 'skey_test_5tecjczxmlxrbtfxhw9'
@@ -15,7 +18,7 @@ omise.api_public = OMISE_PUBLIC
 omise.api_secret = OMISE_SECRET
 
 
-class BasePayment(models.Model):
+class BasePayment(PolymorphicModel):
     """Entry model"""
 
     user = models.ForeignKey(UserProfile, on_delete=models.CASCADE, null=True, blank=True)
@@ -146,3 +149,37 @@ class CashPayment(BasePayment):
             return
 
         self.status = self.Status_choice.PAID
+
+
+# class PaymentInline(StackedPolymorphicInline):
+#     class CashPaymentInline(StackedPolymorphicInline.Child):
+#         model = CashPayment
+
+#     class PromptPayPaymentInline(StackedPolymorphicInline.Child):
+#         model = PromptPayPayment
+
+#     class SCBPaymentInline(StackedPolymorphicInline.Child):
+#         model = SCBPayment
+
+#     class STBPaymentInline(StackedPolymorphicInline.Child):
+#         model = STBPayment
+
+#     class BBLPaymentInline(StackedPolymorphicInline.Child):
+#         model = BBLPayment
+
+#     class BAYPaymentInline(StackedPolymorphicInline.Child):
+#         model = BAYPayment
+
+#     model = BasePayment
+#     child_inlines = (
+#         CashPaymentInline,
+#         PromptPayPaymentInline,
+#         SCBPaymentInline,
+#         STBPaymentInline,
+#         BBLPaymentInline,
+#         BAYPaymentInline,
+#     )
+
+# @admin.register(pay)
+# class PaymentAdmin(PolymorphicInlineSupportMixin, admin.ModelAdmin):
+#     inlines = (PaymentInline,)
