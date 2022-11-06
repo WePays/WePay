1b02e09e997db57b24d0bc026592cf5851908f4a
Tezigudo <godjangg@gmail.com>
2022-11-06 14:20:51 +0700

    ✌️  unfinished work #51 #50

--- WePay/views/__init__.py
+++ WePay/views/__init__.py
@@ -1,12 +1,10 @@
 from .bill import BillView, DetailView, BillCreateView
-from .topic import AddTopicView
 from .payment import PaymentView
 
 
 __all__ = [
     "BillView",
     "BillCreateView",
-    "AddTopicView",
     "PaymentView",
     "DetailView",
 ]
