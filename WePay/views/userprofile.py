2fb95bd7750061bc82c4855cb8b1d9058784c936
Tezigudo <godjangg@gmail.com>
2022-11-10 11:22:46 +0700

    😎Complete automated fetch key feature #50 #63

--- WePay/views/userprofile.py
+++ WePay/views/userprofile.py
@@ -1,10 +1,9 @@
-import omise
 from django.contrib import messages
 from django.shortcuts import redirect, render, reverse
 from django.http import HttpResponseRedirect
 from django.views.generic import DetailView
 
-from ..models import UserProfile
+from ..models import UserProfile, omise
 
 
 class UserProfileView(DetailView):
@@ -16,25 +15,41 @@ class UserProfileView(DetailView):
     def get(self, request, *args, **kwargs):
         user = request.user
         userprofile = UserProfile.objects.get(user=user)
-        chain_key = userprofile.chain_key
-        return render(
-            request, self.template_name, {"user": user, "chain_key": chain_key}
-        )
+        chain_id = userprofile.chain_id
+        return render(request, self.template_name, {"user": user, "chain_id": chain_id})
 
     def post(self, request, *arg, **kwargs):
         user = request.user
-        userprofile = UserProfile.objects.get(user=user)
+        # userprofile = UserProfile.objects.get(user=user)
         display_name = request.POST["display name"]
-        chain_key = request.POST["chain key"]
-        try:
-            omise.Chain.retrieve(chain_key)
-        except omise.errors.NotFoundError:
-            messages.error(request, "Invalid Chain Key")
-            return HttpResponseRedirect(reverse("user-profile:userprofile"))
-        userprofile.chain_key = chain_key
+        # chain_key = request.POST["chain key"]
+        # userprofile.chain_id = chain_key
 
         user.username = display_name
         user.save()
-        userprofile.save()
-
+        # userprofile.save()
+        messages.success(request, f"display name has updated to {display_name}")
         return redirect("/")
+
+
+def fetch_key(request, *args, **kwargs):
+    """Fetch chain key from user profile."""
+    user = request.user
+    userprofile = UserProfile.objects.get(user=user)
+    all_chain = omise.Chain.retrieve()
+
+    for each_chain in all_chain:
+        if each_chain.email == user.email:
+            userprofile.chain_id = each_chain.id
+            userprofile.save()
+            messages.success(request, "Chain key is fetched and found")
+            break
+    else:
+        messages.info(
+            request,
+            "no Chain found, make sure you follow this \
+                <a href='/instruction'>instructions</a> for appling a chain key",
+            extra_tags="safe",
+        )
+
+    return HttpResponseRedirect(reverse("user-profile:userprofile"))
