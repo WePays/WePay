2f399435030553cbd931f4826a14c6948fea8671
Tezigudo <godjangg@gmail.com>
2022-11-11 09:40:29 +0700

    modify setting.py

--- mysite/settings.py
+++ mysite/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = "django-insecure-&mz@g-o-_sg(jwc#nj)d!n3^5stj+w$!(w^8^yb%vuqnjaytg1
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['localhost', '127.0.0.1']
 
 
 # Application definition
