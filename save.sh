6dff9c34eeb9adf61c51f59d24ea047a232d70ee
Tezigudo <godjangg@gmail.com>
2022-11-12 23:25:34 +0700

    deleted save.sh

--- save.sh
+++
@@ -1,7 +0,0 @@
-# # python3 manage.py dumpdata --indent=2 -o data/data.json
-# black *.py
-# read -p "Enter Commit message: " commit_message
-# echo Committing...
-# git commit -m $commit_message
-# echo pushing...
-# git push
