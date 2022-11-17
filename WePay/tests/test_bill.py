d8de0cfb08ef9419b9096d33304ecaab95b03fa4
koonwill <93331101+koonwill@users.noreply.github.com>
2022-11-18 01:16:41 +0700

    Test for add topic

--- WePay/tests/test_bill.py
+++ WePay/tests/test_bill.py
@@ -104,10 +104,18 @@ class BillCreateViewTest(BaseViewTest):
         response = self.client.post(reverse("bills:create"), data=data)
         bill = Bills.objects.last()
         self.assertFalse(bill.is_created)
-        self.assertEqual(response["Location"], "/bill/2/add") # POST success
-        response2 = self.client.get(reverse("bills:success", kwargs={"pk":2}))
-        self.assertEqual(response2["Location"], "/bill/") # After success redirect to bill page
-        self.assertTrue(Bills.objects.get(pk=2).is_created)
+        self.assertEqual(response["Location"], "/bill/2/add")  # POST success
+        response2 = self.client.get(reverse("bills:success", kwargs={"pk": 2}))
+        self.assertEqual(response2["Location"], "/bill/")  # After success redirect to bill page
+        self.assertRedirects(response2, "/bill/", 302, 200)
+        bills = Bills.objects.get(pk=2)
+        topic = Topic.objects.get(title="Toast")
+        self.assertTrue(bills.is_created)
+        self.assertEqual(bills.name, "Est")
+        self.assertEqual(topic.title, "Toast")
+        self.assertEqual(list(topic.user.all()), [self.user1, self.user2])
+        self.assertEqual(topic.price, 2000)
+        self.assertEqual(bills.total_price, 2000)
 
     def test_create_bill_with_more_topic(self):
         """test create a bill with initial topic and add more topic."""
@@ -151,15 +159,15 @@ class DetailViewTest(BaseViewTest):
 
     def test_navigation(self):
         """test navigation after bill object has created"""
-        self.client.login(username="test_header", password="header123")
         response = self.client.get("/bill/1/")
         self.assertEqual(response.status_code, 302)
 
     def test_bill_not_exist(self):
-        """test navigation if go to bill that does not exist"""
-        self.client.login(username="test_header", password="header123")
-        response = self.client.get("/bill/2")
-        self.assertEqual(response.status_code, 301)
+        """test navigation if go to bill that does not exist it will return to bill page"""
+        response = self.client.get(reverse("bills:detail", kwargs={"pk": 1000}))
+        self.assertEqual(response.status_code, 302)
+        self.assertRedirects(response, "/bill/", 302, 200)
+
 
 class AddTopicView(BaseViewTest):
     def setUp(self):
@@ -167,4 +175,56 @@ class AddTopicView(BaseViewTest):
         super(AddTopicView, self).setUp()
 
     def test_add_topic(self):
-        pass
+        data = {
+            "title": "Est",
+            "topic_name": "Toast",
+            "username": [self.user1, self.user2],
+            "topic_price": 2000,
+        }
+        data2 = {
+            "topic_name": "Toasting",
+            "username": [self.user1, self.user2, self.user3],
+            "topic_price": 3000
+        }
+        data3 = {
+            "topic_name": "Tea",
+            "username": [self.user2, self.user3],
+            "topic_price": 4000
+        }
+        response = self.client.post(reverse("bills:create"), data=data)
+        self.assertFalse(Bills.objects.last().is_created)
+        self.assertRedirects(response, "/bill/2/add", 302)
+        response2 = self.client.post(reverse("bills:add", kwargs={"pk": 2}), data=data2)
+        self.assertRedirects(response2, "/bill/2/add", 302)  # POST Success Reload Page
+        # Check that bill is not created when add topic
+        self.assertFalse(Bills.objects.last().is_created)
+        response3 = self.client.post(reverse("bills:add", kwargs={"pk": 2}), data=data3)
+        self.assertRedirects(response3, "/bill/2/add", 302)  # POST Success Reload Page
+        # Check that bill is not created when add topic
+        self.assertFalse(Bills.objects.last().is_created)
+        response4 = self.client.get(reverse("bills:success", kwargs={"pk": 2}))
+        self.assertRedirects(response4, "/bill/", 302)  # POST Success
+        self.assertTrue(Bills.objects.get(pk=2).is_created)  # Bill is created.
+        this_bill = Bills.objects.get(pk=2)
+        first_topic = Topic.objects.get(bill=this_bill, title="Toast")
+        second_topic = Topic.objects.get(bill=this_bill, title="Toasting")
+        third_topic = Topic.objects.get(bill=this_bill, title="Tea")
+        bill_data = {
+            "title": this_bill.name,
+            "topic_name": first_topic.title,
+            "username": list(first_topic.user.all()),
+            "topic_price": first_topic.price
+        }
+        topic_data1 = {
+            "topic_name": second_topic.title,
+            "username": list(second_topic.user.all()),
+            "topic_price": second_topic.price
+        }
+        topic_data2 = {
+            "topic_name": third_topic.title,
+            "username": list(third_topic.user.all()),
+            "topic_price": third_topic.price
+        }
+        self.assertDictEqual(data, bill_data)
+        self.assertDictEqual(data2, topic_data1)
+        self.assertDictEqual(data3, topic_data2)
