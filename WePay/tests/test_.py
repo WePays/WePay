7486cbbd36a02986d0051e773dee54d63398217d
ThanadolU <dolthanadolu@gmail.com>
2022-11-19 21:46:19 +0700

    ❌ still cannot test initialize bill form

--- WePay/tests/test_.py
+++ WePay/tests/test_.py
@@ -5,6 +5,8 @@ from django.test import LiveServerTestCase
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 
 from WePay.models.userprofile import UserProfile
 
@@ -18,10 +20,16 @@ class BillFormTest(LiveServerTestCase):
             user=header, chain_id="acch_test_5tl5qdsa0cbli76hwoj"
         )
         self.header.save()
+
+        user = User.objects.create_user(
+            username="test_user", email="user1@example.com", password="user"
+        )
+        self.user = UserProfile.objects.create(user=user)
+        self.user.save()
         # self.client.force_login(self.header)
         self.browser = webdriver.Chrome()
 
-    # @skip("ERR_CONNECTION_REFUSED")
+    # @skip("Temporalily skip")
     def test_login(self):
         # url to visit
         self.browser.get("http://127.0.0.1:8000/accounts/login/")
@@ -36,24 +44,46 @@ class BillFormTest(LiveServerTestCase):
         password_input.send_keys(self.header.user.password)
 
         # submit form
-        submit_button.send_keys(Keys.RETURN)
+        # submit_button.send_keys(Keys.RETURN)
+        # click the button to login
+        submit_button.click()
 
         # assert "header" in self.browser.page_source
+    @skip("it still not work, i will try later after reading a selenium docs")
+    def test_initialize_bill_form(self):
 
-    # def test_create_bill_form(self):
-    #     self.browser.get('http://127.0.0.1:8000/bill/create/')
+        self.browser.get("http://127.0.0.1:8000/accounts/login/")
 
-    #     title = self.browser.find_element(By.NAME,'title')
-    #     name = self.browser.find_element(By.NAME, 'topic_name')
-    #     price = self.browser.find_element(By.NAME, 'topic_price')
+        # find the elements we need to submit form
+        username_input = self.browser.find_element(By.ID, "id_login")
+        password_input = self.browser.find_element(By.ID, "id_password")
+        submit_button = self.browser.find_element(By.ID, "id_submit")
 
-    #     submit = self.browser.find_element(By.NAME, 'create_title')
+        # populate the form with data
+        username_input.send_keys(self.header.name)
+        password_input.send_keys(self.header.user.password)
 
-    #     title.send_keys('Test Title')
-    #     name.send_keys('Test Food')
-    #     price.send_keys('100')
+        # submit form
+        submit_button.click()
 
-    #     submit.send_keys(Keys.Return)
+        self.browser.implicitly_wait(20)
+
+        self.browser.get('http://127.0.0.1:8000/bill/create/')
+
+        title = self.browser.find_element(By.XPATH, "//input[@id='title']")
+        name = self.browser.find_element(By.ID, 'topic_name')
+        price = self.browser.find_element(By.ID, 'topic_price')
+        assign_to_users = self.browser.find_elements(By.ID, 'username')
+        # assign_to_users = self.browser.find_elements(By.XPATH, )
+
+        create = self.browser.find_element(By.NAME, 'create_title')
+
+        title.send_keys('Test Title')
+        name.send_keys('Test Food')
+        price.send_keys(100)
+        assign_to_users.send_keys('test_user')
+
+        create.send_keys(Keys.Return)
 
     #     assert 'Test Title' in self.browser.page_source
 
