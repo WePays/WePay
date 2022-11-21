6e9a3423b0474c6b0ec6f78ec4bbd19ef2605626
Tezigudo <godjangg@gmail.com>
2022-11-22 00:37:06 +0700

    🚀 Deploying... to heroku1

--- mysite/settings.py
+++ mysite/settings.py
@@ -59,6 +59,7 @@ INSTALLED_APPS = [
 TAILWIND_APP_NAME = "theme"
 
 MIDDLEWARE = [
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     "django.middleware.security.SecurityMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
     "django.middleware.common.CommonMiddleware",
