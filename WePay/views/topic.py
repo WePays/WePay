89af4a9003d476591b96dba5c69e1d5ae41a492b
Tezigudo <godjangg@gmail.com>
2022-11-26 14:19:46 +0700

    🚀Deploying... DEBUGGGG

--- WePay/views/topic.py
+++ WePay/views/topic.py
@@ -1,6 +1,6 @@
 from django.contrib import messages
 from django.contrib.auth.mixins import LoginRequiredMixin
-from django.http import HttpRequest, HttpResponseRedirect
+from django.http import HttpRequest, HttpResponseRedirect, HttpResponse
 from django.shortcuts import get_object_or_404, render
 from django.urls import reverse
 from django.views import generic
@@ -9,9 +9,30 @@ from ..models import Bills, Topic, UserProfile
 
 
 class AddTopicView(LoginRequiredMixin, generic.DetailView):
+    """add a topic to bill
+
+    **Context**
+
+    ``bill``
+        bill that user want to add topic
+
+    ``all_topic``
+        All topic of that bill
+
+    ``lst_user``
+        all user in database
+
+
+    **Template**
+
+    :template:`Wepay/add_topic.html`
+
+    """
     template_name: str = "Wepay/add_topic.html"
 
-    def get(self, request: HttpRequest, *args, **kwargs):
+    def get(self, request: HttpRequest, *args, **kwargs) -> HttpResponse:
+        """get each bill data"""
+
         bill = get_object_or_404(Bills, pk=kwargs["pk"], header__user=request.user)
 
         if bill.is_created:
@@ -26,7 +47,8 @@ class AddTopicView(LoginRequiredMixin, generic.DetailView):
             {"bill": bill, "all_topic": all_topic, "lst_user": lst_user},
         )
 
-    def post(self, request: HttpRequest, *args, **kwargs):
+    def post(self, request: HttpRequest, *args, **kwargs) -> HttpResponse:
+        """add a topic in bill"""
         bill = Bills.objects.get(pk=kwargs["pk"])
         topic_name = request.POST["topic_name"]
         topic_user = request.POST.getlist("username[]")
@@ -42,10 +64,17 @@ class AddTopicView(LoginRequiredMixin, generic.DetailView):
         return HttpResponseRedirect(reverse("bills:add", args=(bill.id,)))
 
 
-def delete_topic(request, pk):
+def delete_topic(request: HttpRequest, pk: int) -> HttpResponse:
+    """delete a topic in bill
+
+    Arguments:
+
+    pk: id of topic to delete
+    """
     topic = get_object_or_404(Topic, pk=pk)
 
     bill = topic.bill
+    # if topic in the bill is 1 user cant delete anytopic now
     if len(Topic.objects.filter(bill=bill)) == 1:
         messages.warning(request, "! Bill must have at least one topic")
         return HttpResponseRedirect(reverse("bills:add", args=(bill.id,)))
