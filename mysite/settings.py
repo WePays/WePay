e6ab27682fc2192f9857de73ab033fb5d4faba72
thanidacwn <92778351+thanidacwn@users.noreply.github.com>
2022-11-29 13:30:31 +0700

    improve 500 page

--- mysite/settings.py
+++ mysite/settings.py
@@ -28,7 +28,7 @@ PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
 SECRET_KEY = config("SECRET_KEY", cast=str, default="missing-secret-key")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = config("DEBUG", cast=bool, default=False)
+DEBUG = config("DEBUG", cast=bool, default=True)
 
 ALLOWED_HOSTS = config("ALLOWED_HOSTS", cast=Csv(), default="")
 
